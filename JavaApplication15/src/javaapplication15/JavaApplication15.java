21897d3ef6f6e8fdd753364508893813200580c6
Leidy <leidy092019@gmail.com>
2019-11-01 14:04:53 -0500

    inicial

---
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,33 @@
+package javaApplication15;
+
+import java.util.Scanner;
+
+public class JavaApplication15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner opc = new Scanner(System.in);
+
+        String nombre,apellido,direccion;
+        int edad,telefono;
+
+       
+        System.out.print("digite su nombre");
+        nombre = opc.nextLine();
+        System.out.println("su nombre es:"+nombre);
+        System.out.print("digite su apellido");
+        apellido = opc.nextLine();
+        System.out.println("su nombre es:"+apellido);
+        System.out.print("digite su direccion");
+        direccion = opc.nextLine();
+        System.out.println("su nombre es:"+direccion);
+
+        System.out.print("digite su edad");
+        edad = opc.nextInt();
+        System.out.println("su nombre es:"+edad);
+        System.out.print("digite su telefono");
+        telefono = opc.nextInt();
+        System.out.println("su nombre es:"+telefono);
+    }}
